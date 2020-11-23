00360f4a37dd887ff1f502f694a3ab8df3623487
glodanm <glodan196915@gmail.com>
2020-11-23 09:58:42 +0200

    my first commit

---
+++ laba3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+char emptyVal(char* input)
+{
+	int n = 0, i = 0;
+	while (n == 0) {
+		if(input [i]+1==input[i+1])
+		{
+			i = i + 1;
+		}
+		else { n = 1; }
+	}
+	return input[i] + 1;
+}
+void main()
+{
+	char mas[27];
+		gets_s(mas, 27);
+	
+		
+		printf("%c", emptyVal(mas));
+
+
+}
+
+
+
